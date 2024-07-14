72f2a69f2b7a4a22e41304bb3b15a521f590d1c0
Balya Elfata <balyaelfata@yahoo.com>
2024-07-14 17:41:33 +0700

    Added WorkoutPlanView

---
+++ Reptor-NC3/Views/WorkoutPlanView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct WorkoutPlanView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
