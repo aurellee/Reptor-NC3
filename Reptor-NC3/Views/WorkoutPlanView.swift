ba3eba60102a2886c0354c7359de03c5ca8d8233
Balya Elfata <balyaelfata@yahoo.com>
2024-07-14 23:41:33 +0700

    Added navigation for Strength, Hypertrphy, and Endurance workout plan view

--- Reptor-NC3/Views/WorkoutPlanView.swift
+++ Reptor-NC3/Views/WorkoutPlanView.swift
@@ -6,86 +6,111 @@ struct WorkoutPlanView: View {
     let reps: Int = 12
     let oneRepMax: Int = 140
     
+    @State var isAllTapped = true
+    @State var isStrengthTapped = false
+    @State var isHypertrophyTapped = false
+    @State var isEnduranceTapped = false
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                // Category
-                ScrollView(.horizontal){
-                    HStack(spacing: 10){
-                        Text("All")
-                            .foregroundStyle(Color.boldRed)
-                            .padding(.vertical, 7)
-                            .padding(.horizontal, 14)
-                            .background(Color.lightRed)
-                            .cornerRadius(19)
-                            .padding(.leading, 16)
-                        Text("Strength")
-                            .foregroundStyle(Color.secondary)
-                            .padding(.vertical, 7)
-                            .padding(.horizontal, 14)
-                            .background(Color(red: 0.96, green: 0.96, blue: 0.96))
-                            .cornerRadius(19)
-                        Text("Hypertrophy")
-                            .foregroundStyle(Color.secondary)
-                            .padding(.vertical, 7)
-                            .padding(.horizontal, 14)
-                            .background(Color(red: 0.96, green: 0.96, blue: 0.96))
-                            .cornerRadius(19)
-                        Text("Endurance")
-                            .foregroundStyle(Color.secondary)
-                            .padding(.vertical, 7)
-                            .padding(.horizontal, 14)
-                            .background(Color(red: 0.96, green: 0.96, blue: 0.96))
-                            .cornerRadius(19)
-                            .padding(.trailing, 16)
-                    }
-                }
-                .padding(.bottom)
-                
-                // Estimated Rep Maxes
-                HStack{
-                    Text("Estimated Rep Maxes")
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                        .padding(.leading, 15)
-                    Spacer()
-                }
-                
-                // Workout Plan Sheet
-                List {
-                    ForEach(1..<13) { item in
-                        HStack(){
-                            Text("\(item) Reps")
-                                .padding(.leading)
-                            Spacer()
-                            Text("\(144 - item*4) kg")
-                            Spacer()
-                            Text("\(103 - item*3)%")
-                                .padding(.trailing)
-                        }
-                        .listRowInsets(EdgeInsets())
-                    }
-                }
-                .listStyle(.plain)
-                
-                // Save History Button
-                Text("Save History")
-                    .foregroundStyle(Color.white)
-                    .padding(.vertical, 14)
-                    .padding(.horizontal, 40)
-                    .background(Color.boldRed)
-                    .cornerRadius(19)
-                
-                Spacer()
-                
-//                // Placeholder variables content
-//                Text("Exercise: \(exercise)")
-//                Text("Weight: \(weight) kg")
-//                Text("Reps: \(reps)")
-//                Text("1RM: \(oneRepMax) kg")
-//                Spacer()
+            if isStrengthTapped {
+                StrengthPlanView()
             }
-            .navigationBarTitle(Text("Workout Plan").font(.largeTitle), displayMode: .inline)
+            else if isHypertrophyTapped {
+                HypertrophyPlanView()
+            }
+            else if isEnduranceTapped {
+                EndurancePlanView()
+            }
+            else {
+                VStack {
+                    // Category
+                    ScrollView(.horizontal){
+                        HStack(spacing: 10){
+                            Text("All")
+                                .foregroundStyle(Color.boldRed)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 14)
+                                .background(Color.lightRed)
+                                .cornerRadius(19)
+                                .padding(.leading, 16)
+                            Text("Strength")
+                                .foregroundStyle(Color.secondary)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 14)
+                                .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                                .cornerRadius(19)
+                                .onTapGesture {
+                                    isStrengthTapped = true
+                                }
+                            Text("Hypertrophy")
+                                .foregroundStyle(Color.secondary)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 14)
+                                .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                                .cornerRadius(19)
+                                .onTapGesture {
+                                    isHypertrophyTapped = true
+                                }
+                            Text("Endurance")
+                                .foregroundStyle(Color.secondary)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 14)
+                                .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                                .cornerRadius(19)
+                                .onTapGesture {
+                                    isEnduranceTapped = true
+                                }
+                                .padding(.trailing, 16)
+                        }
+                    }
+                    .padding(.bottom)
+                    
+                    // Estimated Rep Maxes
+                    HStack{
+                        Text("Estimated Rep Maxes")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .padding(.leading, 15)
+                        Spacer()
+                    }
+                    
+                    // Workout Plan Sheet
+                    List {
+                        ForEach(1..<13) { item in
+                            HStack(){
+                                Text("\(item) Reps")
+                                    .padding(.leading)
+                                Spacer()
+                                Text("\(144 - item*4) kg")
+                                Spacer()
+                                Text("\(103 - item*3)%")
+                                    .padding(.trailing)
+                            }
+                            .listRowInsets(EdgeInsets())
+                        }
+                    }
+                    .listStyle(.plain)
+                    
+                    // Save History Button
+                    Text("Save History")
+                        .foregroundStyle(Color.white)
+                        .padding(.vertical, 14)
+                        .padding(.horizontal, 40)
+                        .background(Color.boldRed)
+                        .cornerRadius(19)
+                    
+                    Spacer()
+                    
+                    //                // Placeholder variables content
+                    //                Text("Exercise: \(exercise)")
+                    //                Text("Weight: \(weight) kg")
+                    //                Text("Reps: \(reps)")
+                    //                Text("1RM: \(oneRepMax) kg")
+                    //                Spacer()
+                }
+                .navigationBarTitle(Text("Workout Plan").font(.largeTitle), displayMode: .inline)
+            }
         }
     }
 }
