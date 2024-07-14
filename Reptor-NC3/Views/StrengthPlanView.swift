d0efefc255fa34de83d55ec3090834ff5f68e454
Balya Elfata <balyaelfata@yahoo.com>
2024-07-14 23:37:07 +0700

    Added views for Strength, Hypertrophy, and Endurance Workout Plan

---
+++ Reptor-NC3/Views/StrengthPlanView.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+
+struct StrengthPlanView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                // Category
+                ScrollView(.horizontal){
+                    HStack(spacing: 10){
+                        Text("All")
+                            .foregroundStyle(Color.secondary)
+                            .padding(.vertical, 7)
+                            .padding(.horizontal, 14)
+                            .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                            .cornerRadius(19)
+                            .padding(.leading, 16)
+                        Text("Strength")
+                            .foregroundStyle(Color.boldRed)
+                            .padding(.vertical, 7)
+                            .padding(.horizontal, 14)
+                            .background(Color.lightRed)
+                            .cornerRadius(19)
+                        Text("Hypertrophy")
+                            .foregroundStyle(Color.secondary)
+                            .padding(.vertical, 7)
+                            .padding(.horizontal, 14)
+                            .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                            .cornerRadius(19)
+                        Text("Endurance")
+                            .foregroundStyle(Color.secondary)
+                            .padding(.vertical, 7)
+                            .padding(.horizontal, 14)
+                            .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                            .cornerRadius(19)
+                            .padding(.trailing, 16)
+                    }
+                }
+                .padding(.bottom)
+                
+                // Estimated Rep Maxes
+                HStack{
+                    Text("Estimated Rep Maxes")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        .padding(.leading, 15)
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+            .navigationBarTitle(Text("Workout Plan").font(.largeTitle), displayMode: .inline)
+        }
+    }
+}
+
+#Preview {
+    StrengthPlanView()
+}
