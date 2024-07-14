016ad63e7fefb223857c02b453f0a00b3b143c38
Balya Elfata <balyaelfata@yahoo.com>
2024-07-14 21:42:33 +0700

    updated Workout Plan View to store exercise type, weight, reps, and 1RM variables

--- Reptor-NC3/Views/WorkoutPlanView.swift
+++ Reptor-NC3/Views/WorkoutPlanView.swift
@@ -1,18 +1,25 @@
 import SwiftUI
 
 struct WorkoutPlanView: View {
+    let exercise: String = "Bench Press"
+    let weight: Int = 100
+    let reps: Int = 12
+    let oneRepMax: Int = 140
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("Exercise: \(exercise)")
+                Text("Weight: \(weight) kg")
+                Text("Reps: \(reps)")
+                Text("1RM: \(oneRepMax) kg")
+                Spacer()
+            }
+            .navigationBarTitle(Text("Workout Plan").font(.largeTitle), displayMode: .inline)
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    WorkoutPlanView()
 }
