906a4c83330562429e12bc31e7e2c66d366b746f
Balya Elfata <balyaelfata@yahoo.com>
2024-07-15 11:13:12 +0700

    Updated Strength Plan View contents

--- Reptor-NC3/Views/StrengthPlanView.swift
+++ Reptor-NC3/Views/StrengthPlanView.swift
@@ -19,7 +19,7 @@ struct StrengthPlanView: View {
                     EndurancePlanView(isAllTapped: $isAllTapped, isStrengthTapped: $isStrengthTapped, isHypertrophyTapped: $isHypertrophyTapped, isEnduranceTapped: $isEnduranceTapped)
                 }
                 else {
-                    // Estimated Rep Maxes
+                    // Recommended Weights
                     HStack{
                         Text("Recommended Weights")
                             .font(.subheadline)
@@ -28,9 +28,84 @@ struct StrengthPlanView: View {
                         Spacer()
                     }
                     
+                    HStack{
+                        Text("112-116 kg")
+                        Spacer()
+                        Text("80-90% of 140 kg")
+                    }
+                    .padding(.horizontal)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                        .foregroundStyle(Color.blackWhite)
+                    
+                    // Recommended Reps
+                    HStack{
+                        Text("Recommended Reps")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .padding(.leading, 15)
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        Text("3-5 reps")
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                        .foregroundStyle(Color.blackWhite)
+                    
+                    // Recommended Sets
+                    HStack{
+                        Text("Recommended Sets")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .padding(.leading, 15)
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        Text("4-6 Sets")
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                        .foregroundStyle(Color.blackWhite)
+                    
+                    // Recommended Rest
+                    HStack{
+                        Text("Recommended Rest")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .padding(.leading, 15)
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        Text("2-3 minutes between sets")
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    
+                    Spacer()
+                    
+                    // Save History Button
+                    Text("Save History")
+                        .foregroundStyle(Color.white)
+                        .padding(.vertical, 14)
+                        .padding(.horizontal, 40)
+                        .background(Color.boldRed)
+                        .cornerRadius(19)
+                    
                     Spacer()
                 }
             }
+            .padding()
             .navigationBarTitle(Text("Workout Plan").font(.largeTitle), displayMode: .inline)
         }
     }
