6d8703569572908f42e940c50500f52c7049c213
Balya Elfata <balyaelfata@yahoo.com>
2024-07-15 11:12:50 +0700

    Updated Strength Plan View contents

--- Reptor-NC3/Views/EndurancePlanView.swift
+++ Reptor-NC3/Views/EndurancePlanView.swift
@@ -19,16 +19,90 @@ struct EndurancePlanView: View {
                         HypertrophyPlanView(isAllTapped: $isAllTapped, isStrengthTapped: $isStrengthTapped, isHypertrophyTapped: $isHypertrophyTapped, isEnduranceTapped: $isEnduranceTapped)
                     }
                     else {
-                    // Estimated Rep Maxes
-                    HStack{
-                        Text("Recommended Weights")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .padding(.leading, 15)
+                        // Recommended Weights
+                        HStack{
+                            Text("Recommended Weights")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                                .padding(.leading, 15)
+                            Spacer()
+                        }
+                        
+                        HStack{
+                            Text("56-184 kg")
+                            Spacer()
+                            Text("40-60% of 140 kg")
+                        }
+                        .padding(.horizontal)
+                        
+                        Rectangle()
+                            .frame(height: 20)
+                            .foregroundStyle(Color.blackWhite)
+                        
+                        // Recommended Reps
+                        HStack{
+                            Text("Recommended Reps")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                                .padding(.leading, 15)
+                            Spacer()
+                        }
+                        
+                        HStack{
+                            Text("15-20 reps")
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                        
+                        Rectangle()
+                            .frame(height: 20)
+                            .foregroundStyle(Color.blackWhite)
+                        
+                        // Recommended Sets
+                        HStack{
+                            Text("Recommended Sets")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                                .padding(.leading, 15)
+                            Spacer()
+                        }
+                        
+                        HStack{
+                            Text("2-3 Sets")
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                        
+                        Rectangle()
+                            .frame(height: 20)
+                            .foregroundStyle(Color.blackWhite)
+                        
+                        // Recommended Rest
+                        HStack{
+                            Text("Recommended Rest")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                                .padding(.leading, 15)
+                            Spacer()
+                        }
+                        
+                        HStack{
+                            Text("30-60 seconds between sets")
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                        
                         Spacer()
-                    }
-                    
-                    Spacer()
+                        
+                        // Save History Button
+                        Text("Save History")
+                            .foregroundStyle(Color.white)
+                            .padding(.vertical, 14)
+                            .padding(.horizontal, 40)
+                            .background(Color.boldRed)
+                            .cornerRadius(19)
+                        
+                        Spacer()
                 }
             }
             .navigationBarTitle(Text("Workout Plan").font(.largeTitle), displayMode: .inline)
