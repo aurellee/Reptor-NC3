5160080fe6ac17d0192fdf692edf778f9d6576be
Balya Elfata <balyaelfata@yahoo.com>
2024-07-16 20:51:51 +0700

    Added time saved down to minutes

--- Reptor-NC3/Views/WorkoutPlanView.swift
+++ Reptor-NC3/Views/WorkoutPlanView.swift
@@ -9,7 +9,7 @@ struct WorkoutPlanView: View {
     let weight: String
     let reps: String
     let oneRepMax: String
-    let date = Date.now.formatted(date: .numeric, time: .omitted)
+    let date = Date.now.formatted(date: .numeric, time: .shortened)
     
     @State var isAllTapped = true
     @State var isStrengthTapped = false
