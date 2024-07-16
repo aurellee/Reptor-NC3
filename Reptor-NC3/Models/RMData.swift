e36d4ab1efb43c6edd9cf802aa4bf5f4929a2a57
Balya Elfata <balyaelfata@yahoo.com>
2024-07-16 17:32:05 +0700

    commeted out id because of adding data error

--- Reptor-NC3/Models/RMData.swift
+++ Reptor-NC3/Models/RMData.swift
@@ -3,15 +3,15 @@ import SwiftData
 
 @Model
 class RMData {
-    var id: UUID
+//    var id: UUID
     var weight: String
     var reps: String
     var exercise: String
     var oneRepMax: String
     var date: String
 
-    init(id: UUID, weight: String, reps: String, exercise: String, oneRepMax: String, date: String) {
-        self.id = id
+    init(/*id: UUID,*/ weight: String, reps: String, exercise: String, oneRepMax: String, date: String) {
+//        self.id = id
         self.weight = weight
         self.reps = reps
         self.exercise = exercise
