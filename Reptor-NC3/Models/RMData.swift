d368bad8ed2ee87c5994aa565193b2ecd080ffe2
Balya Elfata <balyaelfata@yahoo.com>
2024-07-16 10:48:32 +0700

    Removed unused exercises variable and placeholder values

--- Reptor-NC3/Models/RMData.swift
+++ Reptor-NC3/Models/RMData.swift
@@ -3,19 +3,17 @@ import SwiftData
 
 @Model
 class RMData {
-    var weight: String = ""
-    var reps: String = ""
-    var exercise: String = "None"
-    var oneRepMax: String = "0"
-    var exercises: [Exercise] = []
-    var date: String = ""
+    var weight: String
+    var reps: String
+    var exercise: String
+    var oneRepMax: String
+    var date: String
 
-    init(weight: String, reps: String, exercise: String, oneRepMax: String, exercises: [Exercise], date: String) {
+    init(weight: String, reps: String, exercise: String, oneRepMax: String, date: String) {
         self.weight = weight
         self.reps = reps
         self.exercise = exercise
         self.oneRepMax = oneRepMax
-        self.exercises = exercises
         self.date = date
     }
 }
