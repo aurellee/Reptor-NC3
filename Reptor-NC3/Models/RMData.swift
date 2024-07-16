322dce0647e26575e50596c9e29098391211627f
Balya Elfata <balyaelfata@yahoo.com>
2024-07-16 10:17:25 +0700

    Added model variables

--- Reptor-NC3/Models/RMData.swift
+++ Reptor-NC3/Models/RMData.swift
@@ -1,8 +1,21 @@
-//
-//  RMData.swift
-//  Reptor-NC3
-//
-//  Created by Balya Elfata on 16/07/24.
-//
-
 import Foundation
+import SwiftData
+
+@Model
+class RMData {
+    var weight: String = ""
+    var reps: String = ""
+    var exercise: String = "None"
+    var oneRepMax: String = "0"
+    var exercises: [Exercise] = []
+    var date: String = ""
+
+    init(weight: String, reps: String, exercise: String, oneRepMax: String, exercises: [Exercise], date: String) {
+        self.weight = weight
+        self.reps = reps
+        self.exercise = exercise
+        self.oneRepMax = oneRepMax
+        self.exercises = exercises
+        self.date = date
+    }
+}
