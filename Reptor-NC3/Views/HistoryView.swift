e17c6cb4338adf8979574cd4da3e4331bbca509d
Balya Elfata <balyaelfata@yahoo.com>
2024-07-16 10:19:37 +0700

    Adjusted HistoryView to show SwiftData model

--- Reptor-NC3/Views/HistoryView.swift
+++ Reptor-NC3/Views/HistoryView.swift
@@ -6,23 +6,33 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HistoryView: View {
     @ObservedObject private var viewModel = RMCalculator()
+    @Query var rmData: [RMData]
     
     var body: some View {
         NavigationView {
             VStack {
-                List(viewModel.exercises) { exercise in
-                    VStack(alignment: .leading) {
-                        Text("Exercise: \(exercise.exercise)")
-                        Text("Weight: \(exercise.weight) kg")
-                        Text("Reps: \(exercise.reps)")
-                        Text("1RM: \(exercise.oneRepMax) kg")
+//                List(viewModel.exercises) { exercise in
+//                    VStack(alignment: .leading) {
+//                        Text("Exercise: \(exercise.exercise)")
+//                        Text("Weight: \(exercise.weight) kg")
+//                        Text("Reps: \(exercise.reps)")
+//                        Text("1RM: \(exercise.oneRepMax) kg")
+                List{
+                    ForEach(rmData) { data in
+                        VStack(alignment: .leading) {
+                            Text("Exercise: \(data.exercise)")
+                            Text("Weight: \(data.weight) kg")
+                            Text("Reps: \(data.reps)")
+                            Text("1RM: \(data.oneRepMax) kg")
+                        }
+                        .padding()
                     }
-                    .padding()
                 }
-                .navigationBarTitle("History", displayMode: .inline)
+                .navigationBarTitle("History")
             }
         }
     }
