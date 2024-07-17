ae1b4fc63a4cea78d828dd816f1b14138efee5e6
Balya Elfata <balyaelfata@yahoo.com>
2024-07-17 22:33:56 +0700

    added navigation title of current viewed exercise

--- Reptor-NC3/Views/ExerciseDetailView.swift
+++ Reptor-NC3/Views/ExerciseDetailView.swift
@@ -7,28 +7,32 @@ struct ExerciseDetailView: View {
     
     @State var showWorkoutPlan = false
     var body: some View {
-        HStack {
-            Text("1RM")
-                .font(.subheadline)
-                .foregroundStyle(Color.secondary)
-                .padding(.leading, 15)
-            Spacer()
-        }
-        List {
-            ForEach(rmData) { data in
-                if data.exercise == exercise {
-                NavigationLink(destination: WorkoutPlanView(exercise: data.exercise, weight: data.weight, reps: data.reps, oneRepMax: data.oneRepMax, date: data.date, showPopup: .constant(false)))
-                {
-                        HStack {
-                            Text("\(data.oneRepMax) kg")
-                            Spacer()
-                            Text(data.date)
-                                .foregroundStyle(Color.secondary)
+        NavigationStack {
+            HStack {
+                Text("1RM")
+                    .font(.subheadline)
+                    .foregroundStyle(Color.secondary)
+                    .padding(.leading, 15)
+                Spacer()
+            }
+            List {
+                ForEach(rmData) { data in
+                    if data.exercise == exercise {
+                        NavigationLink(destination: WorkoutPlanView(exercise: data.exercise, weight: data.weight, reps: data.reps, oneRepMax: data.oneRepMax, date: data.date, showPopup: .constant(false)))
+                        {
+                            HStack {
+                                Text("\(data.oneRepMax) kg")
+                                Spacer()
+                                Text(data.date)
+                                    .foregroundStyle(Color.secondary)
+                            }
                         }
                     }
                 }
             }
         }
+        .navigationTitle(exercise)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
