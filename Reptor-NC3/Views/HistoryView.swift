4cd76fdd9b741c212f2795644dbeb08d0c0caca7
Balya Elfata <balyaelfata@yahoo.com>
2024-07-17 15:27:05 +0700

    Adjusted HistoryView visuals to match prototype

--- Reptor-NC3/Views/HistoryView.swift
+++ Reptor-NC3/Views/HistoryView.swift
@@ -1,3 +1,46 @@
+//import SwiftUI
+//import SwiftData
+//
+//struct HistoryView: View {
+////    @ObservedObject private var viewModel = RMCalculator()
+//    @Environment(\.modelContext) var modelContext
+//    @Query(sort: [SortDescriptor(\RMData.date, order: .reverse), SortDescriptor(\RMData.exercise, order: .forward)]) var rmData: [RMData]
+//    
+//    var body: some View {
+//        NavigationStack {
+//            VStack {
+//                List{
+//                    ForEach(rmData) { data in
+//                        NavigationLink(
+//                            destination: ExerciseDetailView(exercise: data.exercise))
+//                        {
+//                            VStack(alignment: .leading) {
+//                                Text("Exercise: \(data.exercise)")
+//                                Text("Weight: \(data.weight) kg")
+//                                Text("Reps: \(data.reps)")
+//                                Text("1RM: \(data.oneRepMax) kg")
+//                                Text("Date: \(data.date)")
+//                            }
+//                            .padding()
+//                        }
+//                    }
+//                    .onDelete(perform: { indexes in
+//                        for index in indexes {
+//                            modelContext.delete(rmData[index])
+//                        }
+//                    })
+//                }
+//            }
+//            .navigationBarTitle("Saved Plans")
+//        }
+//    }
+//}
+//
+//
+//#Preview {
+//    HistoryView()
+//}
+
 import SwiftUI
 import SwiftData
 
@@ -6,32 +49,83 @@ struct HistoryView: View {
     @Environment(\.modelContext) var modelContext
     @Query(sort: [SortDescriptor(\RMData.date, order: .reverse), SortDescriptor(\RMData.exercise, order: .forward)]) var rmData: [RMData]
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.boldRed]
+    }
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                List{
-                    ForEach(rmData) { data in
-                        NavigationLink(
-                            destination: ExerciseDetailView(exercise: data.exercise))
-                        {
-                            VStack(alignment: .leading) {
-                                Text("Exercise: \(data.exercise)")
-                                Text("Weight: \(data.weight) kg")
-                                Text("Reps: \(data.reps)")
-                                Text("1RM: \(data.oneRepMax) kg")
-                                Text("Date: \(data.date)")
+            ZStack {
+                Color.primaryBackground
+                    .ignoresSafeArea(.all)
+                
+                ScrollView {
+                    VStack {
+                        ForEach(rmData) { data in
+                            NavigationLink(
+                                destination: ExerciseDetailView(exercise: data.exercise))
+                            {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .foregroundStyle(Color.blackWhite)
+                                        .frame(width: 337, height: 143)
+                                    
+                                    HStack {
+                                        VStack(alignment: .leading) {
+                                            Text("\(data.exercise)")
+                                                .foregroundStyle(Color.primary)
+                                                .fontWeight(.regular)
+                                            
+                                            Spacer()
+                                            
+                                            Text("\(data.oneRepMax) kg")
+                                                .foregroundStyle(Color.primary)
+                                                .fontWeight(.bold)
+                                                .font(.system(size: 34))
+                                        }
+                                        .padding(.vertical, 25)
+                                        
+                                        Spacer()
+                                        
+                                        VStack(alignment: .trailing) {
+                                            Text("\(data.date)")
+                                                .foregroundStyle(Color.primary)
+                                            
+                                            Spacer()
+                                            
+                                            HStack {
+                                                VStack {
+                                                    Text("Weight")
+                                                        .foregroundStyle(Color.primary)
+                                                    Text("\(data.weight)")
+                                                        .foregroundStyle(Color.primary)
+                                                }
+                                                
+                                                VStack {
+                                                    Text("Rep")
+                                                        .foregroundStyle(Color.primary)
+                                                    Text("\(data.reps)")
+                                                        .foregroundStyle(Color.primary)
+                                                }
+                                            }
+                                        }
+                                        .padding(.vertical, 29)
+                                    }
+                                    .padding(.horizontal, 22)
+                                    
+                                }
+                                .padding(.horizontal, 28)
                             }
-                            .padding()
                         }
+                        .onDelete(perform: { indexes in
+                            for index in indexes {
+                                modelContext.delete(rmData[index])
+                            }
+                        })
                     }
-                    .onDelete(perform: { indexes in
-                        for index in indexes {
-                            modelContext.delete(rmData[index])
-                        }
-                    })
+                    .navigationTitle("My Workout Plans")
                 }
             }
-            .navigationBarTitle("Saved Plans")
         }
     }
 }
@@ -40,3 +134,4 @@ struct HistoryView: View {
 #Preview {
     HistoryView()
 }
+
